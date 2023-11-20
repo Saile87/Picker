9b8bd6203c6b216760e59fc880583a0b43aacda5
Elias Breitenbach <elias.b@me.com>
2023-11-20 19:45:27 +0100

    Create a project on github

---
+++ Picker/Picker/PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerApp.swift
+//  Picker
+//
+//  Created by Elias Breitenbach on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
